5fe91f74f8eb261eaed121cce94077536e4468a6
Min1600 <aminhassan00@gmail.com>
2024-08-14 20:43:57 +0100

    Basic structure complete(still needs styling)

--- script.js
+++ script.js
@@ -1,43 +1,67 @@
-const myLibrary = ["a","b","c"];
+const myLibrary = [
+  
+];
 const container = document.querySelector(".container")
 const showButton = document.querySelector(".new")
 const closeButton = document.querySelector(".close")
 const dialog = document.querySelector("dialog")
 const addButton = document.querySelector(".add")
 const input = document.querySelectorAll("input")
+const read = document.getElementById("read")
+const author = document.querySelector("#author")
+const title = document.querySelector("#title")
+const pages = document.querySelector("#pages")
+const card = document.createElement("div")
+const form = document.querySelector("form")
+const readButton = document.createElement("button")
+const removeBtn = document.createElement("button")
 
 function Book(){
-
+this.author = author.value
+this.title = title.value
+this.pages = pages.value
+this.read = read.checked
 }
 
-function check(){
-    let tally = 0
-    input.forEach((item) => {
-     if(item.value || item.checked){
-       tally++
-     }
-    })
-    return tally
-}
-
-function addBookToLibrary(){
-
+Book.prototype.readStatus = function (){
+  input.forEach((item) =>{
+        item.checked=!item.checked
+   })
+  this.read = read.checked
+  console.log(read.checked)
+ newBook(myLibrary[myLibrary.length-1])
 }
 
 function reset(){
     input.forEach((item) =>{
-        item.value="";
-        item.checked = false
-    })
+        item.checked=false
+        item.value = ""
+   })}
+
+function addBookToLibrary(){
+let book = new Book()
+myLibrary.push(book)
 }
 
-function newBook(){
-    let card = document.createElement("div")
+
+function newBook(book){
+    readButton.textContent = "Change read status"
+    removeBtn.textContent = "Remove book from library"
     card.className = "card"
-    card.textContent = "Hi"
+    card.innerHTML = 
+    `<p>Author: ${book.author}</p>
+    <p>Title: ${book.title}</p>
+    <p>Number of Pages: ${book.pages}</p>
+     <p>Read: ${book.read}</p>`
     container.appendChild(card)
+    card.appendChild(readButton)
+    card.appendChild(removeBtn)
 }
 
+
+function removeBook(){
+    card.innerHTML = ""
+}
 
 showButton.addEventListener("click", () =>{
 dialog.showModal()
@@ -45,18 +69,19 @@ reset()
 })
  closeButton.addEventListener("click", (event) => {
     dialog.close();
-    reset()
     event.preventDefault()
  })
 
  addButton.addEventListener("click", (event) =>{
-    let test = check()
-    if(test === 4){
-    newBook()
+   if(form.checkValidity){
+    addBookToLibrary()
+    newBook(myLibrary[myLibrary.length-1])
     event.preventDefault()
     dialog.close()
-    reset()
-    }else{
-        alert("please fill out form")
-    }event.preventDefault()
+   }
  })
+
+
+readButton.addEventListener("click", () => myLibrary[myLibrary.length-1].readStatus())
+
+removeBtn.addEventListener("click", () => removeBook())
