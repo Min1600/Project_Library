9e074e5260a9f213ad854847f8bd45cd55fc82d7
Min1600 <aminhassan00@gmail.com>
2024-08-27 21:02:00 +0100

    update

--- script.js
+++ script.js
@@ -16,51 +16,56 @@ const form = document.querySelector("form")
 const readButton = document.createElement("button")
 const removeBtn = document.createElement("button")
 
-function Book(){
+class Book{
+  constructor(){
 this.author = author.value
 this.title = title.value
 this.pages = pages.value
 this.read = read.checked
-}
+  }
 
-Book.prototype.readStatus = function (){
-  input.forEach((item) =>{
-        item.checked=!item.checked
-   })
-  this.read = read.checked
-  console.log(read.checked)
- newBook(myLibrary[myLibrary.length-1])
-}
-
-function reset(){
+  readStatus(){
     input.forEach((item) =>{
-        item.checked=false
-        item.value = ""
-   })}
+      item.checked=!item.checked
+ })
+this.read = read.checked
+newBook(myLibrary[myLibrary.length-1])
+  }
 
-function addBookToLibrary(){
-let book = new Book()
-myLibrary.push(book)
+
+
+
 }
 
+
+function  reset(){
+  input.forEach((item) =>{
+      item.checked=false
+      item.value = ""
+ })}
+
+function 
+addBookToLibrary(){
+ let book = new Book()
+ myLibrary.push(book)
+ }
 
 function newBook(book){
-    readButton.textContent = "Change read status"
-    removeBtn.textContent = "Remove book from library"
-    card.className = "card"
-    card.innerHTML = 
-    `<p>Author: ${book.author}</p>
-    <p>Title: ${book.title}</p>
-    <p>Number of Pages: ${book.pages}</p>
-     <p>Read: ${book.read}</p>`
-    container.appendChild(card)
-    card.appendChild(readButton)
-    card.appendChild(removeBtn)
+  readButton.textContent = "Change read status"
+  removeBtn.textContent = "Remove book from library"
+  card.className = "card"
+  card.innerHTML = 
+  `<p>Author: ${book.author}</p>
+  <p>Title: ${book.title}</p>
+  <p>Number of Pages: ${book.pages}</p>
+   <p>Read: ${book.read}</p>`
+  container.appendChild(card)
+  card.appendChild(readButton)
+  card.appendChild(removeBtn)
 }
 
-
 function removeBook(){
-    card.innerHTML = ""
+card.innerHTML = ""
 }
 
 showButton.addEventListener("click", () =>{
